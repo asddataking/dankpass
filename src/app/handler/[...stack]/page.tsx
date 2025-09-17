af846cae6db596867ad47eb02a91cf225ea3c6ba
Dan Richmond <40435281+asddataking@users.noreply.github.com>
2025-09-17 18:33:22 -0400

    Debugging Errors

--- src/app/handler/[...stack]/page.tsx
+++ src/app/handler/[...stack]/page.tsx
@@ -1,6 +1,20 @@
 import { StackHandler } from "@stackframe/stack";
 import { stackServerApp } from "@/stack";
 
-export default function StackAuthHandler() {
-  return <StackHandler app={stackServerApp} fullPage={true} />;
+interface StackAuthHandlerProps {
+  params: Promise<{ stack: string[] }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function StackAuthHandler(props: StackAuthHandlerProps) {
+  const params = await props.params;
+  const searchParams = await props.searchParams;
+  
+  return (
+    <StackHandler
+      app={stackServerApp}
+      routeProps={{ params, searchParams }}
+      fullPage={true}
+    />
+  );
 }
